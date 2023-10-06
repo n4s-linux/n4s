358ee5d6695bf80eb126e771abd42e1735030bb5
Jørgen <olsenit@gmail.com>
2023-10-06 21:33:25 +0200

    markdown to logfile format, vitouch difffile tager hensyn til markdown headings og subheadings

--- Applications/difffile.bash
+++ Applications/difffile.bash
@@ -1,17 +1,16 @@
 function difffile() {
-        bn1=$(basename "$1")
-        bn2=$(basename "$2")
+	php /svn/svnroot/Applications/md2logformat.php "$1" > "$1".mdl
+	php /svn/svnroot/Applications/md2logformat.php "$2" > "$2".mdl
         dato=$(echo -n $(date +'%Y-%m-%d %H:%M: '))
 	op="$(whoami)"
-        #diff -c1 "$1" "$2"|grep -vE '^---$'|grep -vE '^---$'|while read i 
-        #diff -u "$1" "$2"|grep -vE '^---$'|grep -vE '^---$'|while read i 
-	diff --changed-group-format='%>' --unchanged-group-format='' "$1" "$2"|while read i
+	diff --changed-group-format='%>' --unchanged-group-format='' "$1".mdl "$2".mdl|while read i
         do
 		echo -e "\t$dato\t+$i ($op)"|sed 's/⌫/⌦⌦⌦/g'|sed 's/ℹ/ℹℹℹ/g'
         done
-	diff --changed-group-format='%<' --unchanged-group-format='' "$1" "$2"|while read i
+	diff --changed-group-format='%<' --unchanged-group-format='' "$1".mdl "$2".mdl|while read i
         do
 		echo -e "\t$dato\t⁒$i ($op)"|sed 's/⌫/⌦⌦⌦/g'|sed 's/ℹ/ℹℹℹ/g'
         done
+	rm "$1".mdl "$2".mdl
 }
 
---
+++ Applications/md2logformat.php
@@ -0,0 +1,19 @@
+<?php
+	require_once("/svn/svnroot/Applications/short.php");
+	$lines = explode("\n",fgc($argv[1]));
+	$curheading=basename($argv[1]);
+	$cursubheading="";
+	foreach ($lines as $curline) {
+		$curline = trim($curline);
+		if (substr($curline,0,1) == "#" && substr($curline,0,2) != "##") {
+			$curheading = trim(substr($curline,1));
+			continue;
+		}
+		else if (substr($curline,0,2) == "##") {
+			$cursubheading = trim(substr($curline,2));
+		}
+		if (!strlen(trim($curline)))continue;
+		$ch = ($cursubheading == "") ? $curheading : $curheading . " - $cursubheading";
+		echo "$ch: $curline\n";
+	}
+?>
