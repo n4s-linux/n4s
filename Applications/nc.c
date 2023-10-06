865c75d03e1979632f266447f044422b58903b73
Jørgen <olsenit@gmail.com>
2023-10-06 23:19:45 +0200

    Arbejdet lidt på ncurses - virker stadig ikke

--- Applications/nc.c
+++ Applications/nc.c
@@ -2,18 +2,22 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+void exitc() {	endwin();exit(0);}
 int main()
 {	int ch = 1000;
 //	WINDOW win WINDOW *newwin(int nlines, int ncols, int begin_y, int begin_x);
 	WINDOW *win;
-
+	WINDOW *menu = newwin(3,80,0,0);
+	WINDOW *main = newwin(25,80,4,0);
 	initscr();			/* Start curses mode 		*/
-	raw();				/* Line buffering disabled	*/
+	raw();			/* Line buffering disabled	*/
+wborder(menu, '-', '-', '-','-','-','-','-','-');
+wborder(main, '-', '-', '-','-','-','-','-','-');
 	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
 	noecho();			/* Don't echo() while we do getch */
 
 	while (ch != 0) {
+		if (ch == 3) exitc();
 		if(ch == KEY_F(10)) {		/* Without keypad enabled this will */
 			printw("F1 Key pressed");/*  not get to us either	*/
 			endwin();
@@ -21,17 +25,17 @@ int main()
 		}
 
 		attron(A_BOLD);
-		mvaddstr(0,0,("Start"));
-		mvaddstr(0,20,getenv("LEDGER_BEGIN"));
-		mvaddstr(1,0,("Slut"));
-		mvaddstr(1,20,getenv("LEDGER_END"));
+		mvwaddstr(main,0,0,("Start"));
+		mvwaddstr(main,0,20,getenv("LEDGER_BEGIN"));
+		mvwaddstr(main,1,0,("Slut"));
+		mvwaddstr(main,1,20,getenv("LEDGER_END"));
 		char foobar[80];
 		sprintf(foobar,"hejsa %u   ",ch);
-		mvaddstr(3,10,foobar);
+		mvwaddstr(main,3,10,foobar);
 		attroff(A_BOLD);
-
-	refresh();			/* Print it on to the real screen */
-    	ch = getch();			/* Wait for user input */
+		wrefresh(main);			/* Print it on to the real screen */
+		wrefresh(menu);			/* Print it on to the real screen */
+		ch = wgetch(main);			/* Wait for user input */
 	}
 	endwin();			/* End curses mode		  */
 	return 0;
