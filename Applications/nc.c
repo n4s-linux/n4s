e1c9f1e4e1c8449677015e7b4f62feddbca1410f
Jørgen <olsenit@gmail.com>
2023-10-08 10:56:01 +0200

    Diverse

--- Applications/nc.c
+++ Applications/nc.c
@@ -1,42 +1,89 @@
-
-#include <ncurses.h>
 #include <stdio.h>
-#include <stdlib.h>
-void exitc() {	endwin();exit(0);}
+#include <ncurses.h>
+
+#define WIDTH 30
+#define HEIGHT 10 
+
+int startx = 0;
+int starty = 0;
+
+char *choices[] = { 
+			"Choice 1",
+			"Choice 2",
+			"Choice 3",
+			"Choice 4",
+			"Exit",
+		  };
+int n_choices = sizeof(choices) / sizeof(char *);
+void print_menu(WINDOW *menu_win, int highlight);
+
 int main()
-{	int ch = 1000;
-//	WINDOW win WINDOW *newwin(int nlines, int ncols, int begin_y, int begin_x);
-	WINDOW *win;
-	WINDOW *menu = newwin(3,80,0,0);
-	WINDOW *main = newwin(25,80,4,0);
-	initscr();			/* Start curses mode 		*/
-	raw();			/* Line buffering disabled	*/
-wborder(menu, '-', '-', '-','-','-','-','-','-');
-wborder(main, '-', '-', '-','-','-','-','-','-');
-	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
-	noecho();			/* Don't echo() while we do getch */
+{	WINDOW *menu_win;
+	int highlight = 1;
+	int choice = 0;
+	int c;
 
-	while (ch != 0) {
-		if (ch == 3) exitc();
-		if(ch == KEY_F(10)) {		/* Without keypad enabled this will */
-			printw("F1 Key pressed");/*  not get to us either	*/
-			endwin();
-			return 0;
+	initscr();
+	clear();
+	noecho();
+	cbreak();	/* Line buffering disabled. pass on everything */
+	startx = 0;
+	starty = 0;
+	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+	keypad(menu_win, TRUE);
+	refresh();
+	print_menu(menu_win, highlight);
+	while(1)
+	{	c = wgetch(menu_win);
+		switch(c)
+		{	case KEY_UP:
+				if(highlight == 1)
+					highlight = n_choices;
+				else
+					--highlight;
+				break;
+			case KEY_DOWN:
+				if(highlight == n_choices)
+					highlight = 1;
+				else 
+					++highlight;
+				break;
+			case 10:
+				choice = highlight;
+				break;
+			default:
+				mvprintw(24, 0, "Charcter pressed is = %3d Hopefully it can be printed as '%c'", c, c);
+				refresh();
+				break;
 		}
-
-		attron(A_BOLD);
-		mvwaddstr(main,0,0,("Start"));
-		mvwaddstr(main,0,20,getenv("LEDGER_BEGIN"));
-		mvwaddstr(main,1,0,("Slut"));
-		mvwaddstr(main,1,20,getenv("LEDGER_END"));
-		char foobar[80];
-		sprintf(foobar,"hejsa %u   ",ch);
-		mvwaddstr(main,3,10,foobar);
-		attroff(A_BOLD);
-		wrefresh(main);			/* Print it on to the real screen */
-		wrefresh(menu);			/* Print it on to the real screen */
-		ch = wgetch(main);			/* Wait for user input */
-	}
-	endwin();			/* End curses mode		  */
+		print_menu(menu_win, highlight);
+		if(choice != 0)	/* User did a choice come out of the infinite loop */
+			break;
+	}	
+	mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
+	clrtoeol();
+	refresh();
+	endwin();
 	return 0;
+}
+
+
+void print_menu(WINDOW *menu_win, int highlight)
+{
+	int x, y, i;	
+
+	x = 2;
+	y = 2;
+	box(menu_win, 0, 0);
+	for(i = 0; i < n_choices; ++i)
+	{	if(highlight == i + 1) /* High light the present choice */
+		{	wattron(menu_win, A_REVERSE); 
+			mvwprintw(menu_win, y, x, "%s", choices[i]);
+			wattroff(menu_win, A_REVERSE);
+		}
+		else
+			mvwprintw(menu_win, y, x, "%s", choices[i]);
+		++y;
+	}
+	wrefresh(menu_win);
 }
