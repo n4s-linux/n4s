b3d1e6846361ca437c53e901663f79751de96fe6
Jørgen <olsenit@gmail.com>
2023-10-14 17:03:12 +0200

    Arbejdet videre på 999

--- Applications/scrips/999_Periodresult.bash
+++ Applications/scrips/999_Periodresult.bash
@@ -4,3 +4,22 @@ res=0
 fi
 dato=$(date --date="$LEDGER_END -1 day" +%Y-%m-%d)
 echo -e "$dato Periodens resultat\n\tEgenkapital:Periodens resultat  $res\n\tResultatdisponering:Periodens resultat\n\n"
+
+exit
+
+
+if [ "$noend" == "1" ]; then
+	exit
+fi
+echo "$LEDGER_BEGIN Overført saldo"
+LEDGER_END=$(date --date="$LEDGER_BEGIN -1 day" +%Y-%m-%d) LEDGER_BEGIN=1970/1/1 noend=1 ledger --no-pager -B -f $tpath/curl b --flat ^Aktiver ^Passiver ^Egenkapital ^Fejlkonto --no-total --balance-format="%(account)\t%(total)\n"|while read i
+do
+	konto=$(echo "$i"|awk '{print $1}')
+	belob=$(echo "$i"|awk '{print $2}')
+	re='^[+-]?[0-9]+([.][0-9]+)?$'
+	if  [[ $belob =~ $re ]] ; then
+		echo -e "\t$konto  $belob"
+	fi
+
+done
+echo -e "\tDifferencer\n\n"
