ecb2137cdb64d8c036f929ad204514298ae2a7a0
Jørgen <olsenit@gmail.com>
2023-10-16 18:09:02 +0200

    Mere læsbart diff-output (korrekt rækkefølge)

--- Applications/difffile.bash
+++ Applications/difffile.bash
@@ -3,14 +3,10 @@ function difffile() {
 	php /svn/svnroot/Applications/md2logformat.php "$2" > "$2".mdl
         dato=$(echo -n $(date +'%Y-%m-%d %H:%M: '))
 	op="$(whoami)"
-	diff --changed-group-format='%>' --unchanged-group-format='' "$1".mdl "$2".mdl|while read i
-        do
-		echo -e "\t$dato\t+$i ($op)"|sed 's/⌫/⌦⌦⌦/g'|sed 's/ℹ/ℹℹℹ/g'
-        done
-	diff --changed-group-format='%<' --unchanged-group-format='' "$1".mdl "$2".mdl|while read i
-        do
-		echo -e "\t$dato\t⁒$i ($op)"|sed 's/⌫/⌦⌦⌦/g'|sed 's/ℹ/ℹℹℹ/g'
-        done
+	diff --unified=0 "$1".mdl "$2".mdl|while read i
+	do
+			echo -e "\t$dato joo\t$i"
+	done
 	rm "$1".mdl "$2".mdl
 }
 
