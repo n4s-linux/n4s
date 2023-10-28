d3f3d0c0e6749b93e8a487ff12c57fd3891b89ca
Jørgen <olsenit@gmail.com>
2023-10-28 20:29:31 +0200

    Mailstuff fjernes

--- Applications/sortmail.bash
+++
@@ -1,9 +0,0 @@
-# My Heritage er spam
-	rt ls -i "Status='new' and (
-		Requestor.EmailAddress LIKE 'myheritage' or 
-		Requestor.EmailAddress like 'googlebase-noreply@google.com' or
-		Requestor.EmailAddress like 'updates-noreply@linkedin.com'
-
-	)"|rt edit - set Queue=Spam set Status=rejected
-
-
