71fb2b59f08119ee6c3081b6c0abcbbb1d267be2
Jørgen <olsenit@gmail.com>
2023-11-06 10:48:51 +0100

    matematisk input tillades i newl

--- Applications/key.php
+++ Applications/key.php
@@ -690,29 +690,7 @@ function get_amount($account,$bal) {
 	}
 	return array("formula"=>$form,"calculated"=>number_format($result,2,".",""));
 }
-function evalmath($equation)
-{
-	if ($equation == "") return "";
-    $result = 0;
-    // sanitize imput
-    $equation = preg_replace("/[^a-z0-9+\-.*\/()%]/","",$equation);
-    // convert alphabet to $variabel
-    $equation = preg_replace("/([a-z])+/i", "\$$0", $equation);
-    // convert percentages to decimal
-    $equation = preg_replace("/([+-])([0-9]{1})(%)/","*(1\$1.0\$2)",$equation);
-    $equation = preg_replace("/([+-])([0-9]+)(%)/","*(1\$1.\$2)",$equation);
-    $equation = preg_replace("/([0-9]{1})(%)/",".0\$1",$equation);
-    $equation = preg_replace("/([0-9]+)(%)/",".\$1",$equation);
-    if ( $equation != "" ){
-	if ($equation == "0") return 0;
-        $result = @eval("return " . trim($equation) . ";" );
-    }
-    if ($result == null) {
-        throw new Exception("Unable to calculate equation");
-    }
-    return $result;
-   // return $equation;
-}
+require_once("/svn/svnroot/Applications/math.php");
 function has_children($array) {
 	$children = 0;
 	foreach ($array as $key) {
