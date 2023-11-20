b1fa55f0dfe4fc4c4ae4b029c7ff83b2104d6f75
Jørgen <olsenit@gmail.com>
2023-11-20 12:06:51 +0100

    Nextnumber - næste transaktionsnummer og cashbook nr

---
+++ Applications/nextnumber.php
@@ -0,0 +1,20 @@
+<?php
+	function getnextnumber($path) {
+		if (!file_exists("$path/.nextnumber")) {
+			$nextnumber = 1; file_put_contents("$path/.nextnumber",1); 
+		}
+		else {
+			$nextnumber = trim(file_get_contents("$path/.nextnumber"));
+		}
+		return $nextnumber;
+	}
+	function getnextcbnumber($path) {
+		if (!file_exists("$path/.nextcbnumber")) {
+			$nextnumber = 1; file_put_contents("$path/.nextcbnumber",1); 
+		}
+		else {
+			$nextnumber = trim(file_get_contents("$path/.nextcbnumber"));
+		}
+		return $nextcbnumber;
+	}
+?>
