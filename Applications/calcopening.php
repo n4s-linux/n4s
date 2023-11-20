5e0a3bfba505a2a9bb9f21522889ac70b7a925f2
Jørgen <olsenit@gmail.com>
2023-11-20 12:07:21 +0100

    Fjernet html output fra åbning

--- Applications/calcopening.php
+++ Applications/calcopening.php
@@ -1,7 +1,8 @@
 <?php
+$begin = getenv("LEDGER_BEGIN");
 // Formål: Beregning af rente på baggrund af aktuel ledger
 // Status: Ikke funktionel
-echo "Genererer åbningsposter...\n";
+echo "Opdaterer åbningsposter $begin...";
 $accounts = "";
 $saldo = array();
 require_once("/svn/svnroot/Applications/short.php");
@@ -26,7 +27,6 @@ while ($line = fgetcsv($fd,null,",","\"","\\")) { // GET DATA
 	else
 		$saldo[$konto] = $belob;
 }
-$begin = getenv("LEDGER_BEGIN");
 $s = "$begin Overført saldo\n";
 $y = 0;
 foreach ($saldo as $konto => $cursaldo) {
@@ -40,32 +40,4 @@ if ($y != 0)
 $s .= "\n";
 $tpath = getenv("tpath");
 file_put_contents("$tpath/.Åbning_$begin.ledger",$s);
-$aktiver = "<table border=1>";
-$passiver = "<table border=1>";
-$egenkapital = "<table border=1>";
-$fejlkonto = "<table border=1>";
-foreach ($saldo as $konto => $cursaldo) {
-	$y += $cursaldo;
-	if (intval($cursaldo) == 0) continue;
-	$sk = $konto;
-	$cursaldo = number_format($cursaldo,2,",",".");
-	if (substr($konto,0,strlen("Aktiver")) == "Aktiver") $aktiver .= "<tr><td>$sk</td><td><p align=right>$cursaldo</p></td>";
-	else if (substr($konto,0,strlen("Aktiver")) == "Aktiver") $aktiver .= "<tr><td>$sk</td><td><p align=right>$cursaldo</p></td>";
-	else if (substr($konto,0,strlen("Passiver")) == "Passiver") $passiver.= "<tr><td>$sk</td><td><p align=right>$cursaldo</p></td>";
-	else if (substr($konto,0,strlen("Egenkapital")) == "Egenkapital") $egenkapital.= "<tr><td>$sk</td><td><p align=right>$cursaldo</p></td>";
-	else if (substr($konto,0,strlen("Fejlkonto")) == "Fejlkonto") $fejlkonto.= "<tr><td>$sk</td><td><p align=right>$cursaldo</p></td>";
-}
-$aktiver .= "</table>";
-$passiver .= "</table>";
-$egenkapital .= "</table>";
-$fejlkonto .= "</table>";
-$ft = "<table border=1>";
-$ft .= "<tr><td><b>Aktiver</b><br>$aktiver</td>";
-$ft .= "</tr><tr><td><b>Passiver</b><br>$passiver</td></tr>";
-$ft .= "<tr><td><b>Egenkapital</b><br>$egenkapital</td>";
-$ft .= "</tr><tr><td><b>Fejlkonto</b><br>$fejlkonto</td></tr>";
-$ft .= "</table>";
-file_put_contents("/home/$op/tmp/ft.html",$ft);
-require_once("/svn/svnroot/Applications/proc_open.php");
-echo "Tast p for at se primobalance\n"
 ?>
