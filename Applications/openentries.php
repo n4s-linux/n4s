49c74f79792c8df0c7ea6ce37e1de7791e055eaa
Jørgen <olsenit@gmail.com>
2023-11-25 18:49:26 +0100

    Openentries omskrevet til at få data med fra hele bogen via csv export

--- Applications/openentries.php
+++ Applications/openentries.php
@@ -1,14 +1,13 @@
 <?php
 function findmatch($open,$fejl,$maxtimediff = 30) {
 	global $tpath;
-	foreach ($open as $curopen) {
-		foreach ($curopen['Transactions'] as $curopentrans) {
+	foreach ($open as $key => $val) {
+		foreach ($val['Transactions'] as $curopentrans) {
 			foreach ($fejl as $curfejl) {
 				if ($curopentrans['Amount'] == -$curfejl['Amount']) {
 					$md = md5(json_encode($curopentrans) . json_encode($curfejl));
 					if (file_exists("$tpath/.openentries/$md")) continue;
-					$diff = abs(strtotime($fejl['Date']) - strtotime($curopentrans['Date']));
-					return array('Timediff'=>$td,'Open'=>$curopentrans,'Fejl'=>$curfejl,'Ignorefile'=>"$tpath/.openentries/$md");
+					return array('Open'=>$curopentrans,'Fejl'=>$curfejl,'Ignorefile'=>"$tpath/.openentries/$md");
 				}
 			}
 		}
@@ -22,17 +21,18 @@ function getopen($dk) {
 	return $removezeroes;
 }
 function removezero($d) {
-	foreach ($d as &$curd) {
-		$sum = 0;
+	foreach ($d as $key => &$val) {
 		$transactions = array();
-		foreach ($curd['Transactions'] as $curtrans) {
+		$sum = 0;
+		foreach ($val['Transactions'] as &$curtrans) {
 			$sum += $curtrans['Amount'];
 			array_push($transactions,$curtrans);
 			if (intval($sum) == 0) {
 				$transactions = array();
 			}
 		}
-		$curd['Transactions'] = $transactions;
+		$retval[$key]['Transactions'] = $transactions;
+		$retval[$key]['Sum'] = $sum;
 	}
 	return $d;
 }
@@ -55,46 +55,40 @@ function groupbyacc($dk) {
 			$r[$c['Account']]['Sum'] = $c['Amount'];
 	}
 	// remove empty
-	$retval = array();
-	foreach ($r as $curr) {
+	foreach ($r as $key=>$curr) {
 		if (!intval($curr['Sum']) == 0)
-			array_push($retval,$curr);
+			$retval[$key] = $curr;
 	}
 	return ($retval);
 }
 function getfejl($x) {
 	$fejl = array();
-	foreach ($x as $curx) {
-		$i = 0;
-		foreach ($curx['Transactions'] as $curtrans) {
-			if (stristr($curtrans['Account'],'Fejlkonto')) {
-				$curtrans['Date'] = $curx['Date'];
-				$curtrans['Description'] = $curx['Description'];
-				if (isset($curx['Reference'])) $curtrans['Reference'] = $curx['Reference'];
-				$curtrans['Filename'] = $curx['Filename'];
-				array_push($fejl,$curtrans);
-				$i++;
-			}
+	foreach ($x as $curtrans) {
+		if (stristr($curtrans['Account'],'Fejlkonto')) {
+			array_push($fejl,$curtrans);
 		}
 	}
+	
 	return $fejl;
 }
 function getdebcred($x) {
 		$open = array();
-		foreach ($x as $curx) {
-			$i=0;
-			foreach ($curx['Transactions'] as $curtrans) {
+		$i = 0;
+		foreach ($x as $curtrans) {
 				if (stristr($curtrans['Account'],'Debitorer') || stristr($curtrans['Account'],'Kreditorer')) {
-					$curtrans['OpenTrans'] = $i;
-					$curtrans['Date'] = $curx['Date'];
-					$curtrans['Description'] = $curx['Description'];
-					if (isset($curx['Reference'])) $curtrans['Reference'] = $curx['Reference'];
-					$curtrans['Filename'] = $curx['Filename'];
+					$curtrans['Filename'] = gettag($curtrans,"Filename");
 					array_push($open,$curtrans);
 				}
 				$i++;
-			}
 		}
 		return $open;
+}
+function gettag($trans,$tag) {
+	if (!isset($trans['tags'])) return -1;
+	$x = explode("$tag:",$trans['tags']);
+	if (!isset($x[1])) {
+		 return -2;
+	}
+	return trim(explode("||||",$x[1])[0]);
 }
 ?>
