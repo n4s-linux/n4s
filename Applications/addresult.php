47b3ca9f1b947460211f963baca4aa7ebc833f2c
Jørgen <olsenit@gmail.com>
2023-11-25 18:46:23 +0100

    Addresult = virtuelle transaktioner

--- Applications/addresult.php
+++ Applications/addresult.php
@@ -3,9 +3,10 @@
 		$income = 0;
 		foreach ($x as &$ct) {
 			foreach ($ct['Transactions'] as $curtrans) {
+				if ($curtrans['Amount'] == 0) continue;
 				if (substr($curtrans['Account'],0,strlen("Indtægter")) == "Indtægter" || substr($curtrans['Account'],0,strlen("Udgifter")) == "Udgifter") {
-					$ct['Transactions'][] = array('Account'=>'Egenkapital:Periodens resultat','Amount'=>$curtrans['Amount']);
-					$ct['Transactions'][] = array('Account'=>'Resultatdisponering:Periodens resultat','Amount'=>-$curtrans['Amount']);
+					$ct['Transactions'][] = array('Account'=>'Egenkapital:Periodens resultat','Amount'=>$curtrans['Amount'],'id'=>"virt");
+					$ct['Transactions'][] = array('Account'=>'Resultatdisponering:Periodens resultat','Amount'=>-floatval($curtrans['Amount']),'id'=>"virt");
 				}
 			}
 		}
