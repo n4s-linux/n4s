ba34243331b3e6579c9b9d02c7e45a745d638491
Jørgen <olsenit@gmail.com>
2023-11-30 17:16:22 +0100

    Gamle rente scripts ubrugelige

--- Applications/interest.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-noend=1 LEDGER_BEGIN=1970/1/1 LEDGER_END=$(echo -n $(date +%Y-%m-%d -d "tomorrow")) php /svn/svnroot/Applications/key.php ledger csv ^Aktiver ^Passiver > ~/tmp/interest.out
-php /svn/svnroot/Applications/interest.php
