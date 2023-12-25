91f3d57c12b03e342fd980f3296112e1076cfada
Jørgen <olsenit@gmail.com>
2023-12-26 00:04:30 +0100

    Funktioner udskilles til global kørsel

--- Applications/htmlreport.php
+++ Applications/htmlreport.php
@@ -1,4 +1,13 @@
 <?php
+function shorten($s) { return $s;}                                                                                                                                                                                                     
+?>
+<?php
+function showfejlkonto() {?>
+<div class="alert alert-danger" role="alert">
+  Der er poster på fejkontoen - det betyder at vi mangler oplysninger se venligst kontospecifikationerne
+</div>
+<?php }
+require_once("/svn/svnroot/Applications/accountheader.php");
 require_once("/svn/svnroot/Applications/rewrite.php");
 require_once("/svn/svnroot/Applications/short.php");
 $op = exec("whoami");
@@ -217,59 +226,6 @@ function getdata($begin,$end) {
 		file_put_contents("/home/joo/tmp/notes.json",json_encode($notes,JSON_PRETTY_PRINT));
 	}
 	function prettynum($a) { return "<p align=right>" . number_format($a,0,",",".") . "</p>";}
-?>
-<?php
-function printheader($parameter = "Saldobalance",$orientation="Portrait") {
-global $tpath;
-global $begin;
-global $end;
-global $realend;
-?><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<html charset=utf8>
-<style type="text/css" media="print">
-@page {
-    size: auto;   /* auto is the initial value */
-    margin: 0;  /* this affects the margin in the printer settings */
-}
-@media print {
-<?php if ($parameter == "Saldobalance") {?>
-	div {
-    break-inside: avoid;
-}
-	table {
-    break-inside: avoid;
-	}
-<?php }?>
-    table tbody tr td:before,
-    table tbody tr td:after {
-        content: "";
-        height: 4px;
-        display: block;
-    }
-@media print{@page {size: <?php echo $orientation;?>}}
-</style>
-<?php 
-
-echo "<p align=right>";
-echo "København " . date("Y-m-d") . "</p><center><br>";
-for ($i = 0;$i<12;$i++)echo "<br>";
-require_once("/svn/svnroot/Applications/prettyname.php");
-echo "<br><b>$parameter</b><br>" . prettyname($tpath) . "</center>";
-echo "<center><br><b>Periode:</b><br>";
-$realend = date("Y-m-d", strtotime("$end -1 day"));
-echo "$begin - $realend";
-echo "</center>";
-echo "<p style=\"page-break-after: always;\">&nbsp;</p>";
-
-}
-function shorten($s) { return $s;}
-?>
-<?php
-function showfejlkonto() {?>
-<div class="alert alert-danger" role="alert">
-  Der er poster på fejkontoen - det betyder at vi mangler oplysninger se venligst kontospecifikationerne
-</div>
-<?php }
 function getw($col) {
 	if ($col == "KontoN1")
 		return 200;
