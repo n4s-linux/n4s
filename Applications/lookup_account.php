eabdf03f6b5182f7c9f9316200028c5c453d5d85
Jørgen <olsenit@gmail.com>
2024-01-05 09:58:18 +0100

    Forsimplet lookup account

--- Applications/lookup_account.php
+++ Applications/lookup_account.php
@@ -2,17 +2,9 @@
 require_once("/svn/svnroot/Applications/print_array.php");
 function lookup_acc($accounts,$bal,$alias = "",$multi = "--multi") {
 	$path = getenv("tpath");
-	$hist = "$path/.acchist";
-    system("stty -icanon -echo");
-	$curpos = $accounts;
 	$accountstring = "";
-
-	//system("clear");
-	$v = $curpos;
-	$keyz = print_array($curpos);
-	$lvl = 0;
 	ob_start();
-	$cmd = ("(php /svn/svnroot/Applications/newl.php accounts;cat /svn/svnroot/Libraries/Kontoplan.txt)|sort|uniq|fzf --header='vælg konto for $alias' --scrollbar=* $multi --margin 1% --padding 1% --border --preview-label='Seneste posteringer' --preview-window 55%:bottom --preview 'LEDGER_SORT=date LEDGER_PAYEE_WIDTH=15 php /svn/svnroot/Applications/newl.php register ^\"{}\" |tac'");
+	$cmd = ("(echo NY;updatealiases= php /svn/svnroot/Applications/newl.php accounts;cat /svn/svnroot/Libraries/Kontoplan.txt)|sort|uniq|fzf --header-first --header-lines=1 --header='$alias' --scrollbar=* $multi --margin 1% --padding 1% --border --preview-label='Seneste posteringer' --preview-window 35%:bottom --preview 'LEDGER_SORT=date LEDGER_PAYEE_WIDTH=15 updatealiases= php /svn/svnroot/Applications/newl.php register ^\"{}\" |tac'");
 	system($cmd);
 	$accountstring = trim(ob_get_clean());
 	if ($accountstring == "") die("Afbrudt lookup_account intet valgt");
@@ -27,17 +19,5 @@ function lookup_acc($accounts,$bal,$alias = "",$multi = "--multi") {
 			fclose($fd);
 		}
 	}
-	else {
-		$curpos = $accountstring;
-		$lvl++;
-	}
-
-		$v = $curpos;
-		//echo "$accountstring\n";
-
-	
-
-
-    system("stty sane");
 	return $accountstring;
 }
