50e2fcd095491c807220cb8b1c0f0e650301e07a
Jørgen <olsenit@gmail.com>
2024-01-21 04:35:41 +0100

    Udkast til ny fzf-baseret regnskabsmenu med indtil videre gode og nyttige views

---
+++ Applications/menu.php
@@ -0,0 +1,116 @@
+<?php
+$op = exec("whoami");
+$mode = screensizemode();
+$begin = getenv("LEDGER_BEGIN");
+$end = getenv("LEDGER_END");
+if (getenv("tpath") == "") die("Kræver tpath\n");
+$hovedmenu = array("Saldobalance","Kontokort","Indstillinger","Udskrifter","Import","Eksport");
+
+if (!isset($argv[1]) ||$argv[1] == "") {
+	menu($hovedmenu,"n4s","Sneakpreview");
+}
+else if ($argv[1] == "Saldobalance") {
+	$a = getaccounts();
+	menu($a,"Regnskabstal $begin - $end","Væsentlige poster","php /svn/svnroot/Applications/menu.php Væsentligeposter $begin $end {}");
+	
+}
+else if ($argv[1] == "Preview") {
+	if ($argv[2] == "Saldobalance")
+		echo getpivot();
+}
+else if ($argv[1] == "Væsentligeposter") {
+	$konto = trim($argv[4]);
+	$x = explode(" ",$konto);
+	if (count($x) > 2) {
+		unset($x[0]);unset($x[1]);
+	}
+	else
+		$konto = trim($argv[4]);
+
+	$konto = implode(" ",$x);
+	$konto = str_replace(" ",".",$konto);
+	$konto = str_replace("&","\\&",$konto);
+	$konto = str_replace("%",".",$konto);
+	getepictrans($konto);
+}
+else
+	echo "$argv[1] unhandled\n";
+
+
+
+
+
+function menu($options,$header,$previewlabel,$previewcommand = "php /svn/svnroot/Applications/menu.php Preview {}") {
+$uid = md5($header) ;//. time();
+$str = "";
+foreach ($options as $curoption) {
+	$str .= "$curoption\n";
+}
+$cmd = "echo \"$str\" > ~/tmp/.mymenu_$uid;cat ~/tmp/.mymenu_$uid|fzf  --preview='$previewcommand' --header-first --header=\"$header\" --scrollbar=* --margin 2% --padding 1% --border --preview-label='$previewlabel' --preview-window 50%:right --bind 'load:reload-sync(sleep 1;cat ~/tmp/.mymenu_$uid)+unbind(load)'";
+system($cmd);
+}
+function getaccounts() {
+	$tlas = gettlas();
+	ob_start();
+	$uid = time();
+	global $op;
+	system("php /svn/svnroot/Applications/newl.php print > ~/tmp/curl_$uid");
+	foreach ($tlas as $curtla) {
+		echo strtoupper($curtla) . "\n--------------------\n\n";
+		system("ledger -f ~/tmp/curl_$uid balance -E --flat \"^$curtla\"");
+		echo "\n";
+	}
+	unlink("/home/$op/tmp/curl_$uid");
+	return array_reverse(explode("\n",ob_get_clean()));
+}
+function gettlas() { // tla = top level account
+	ob_start();
+	system("php /svn/svnroot/Applications/newl.php print |hledger --depth=2 -f /dev/stdin accounts -E");
+	$r = array_filter(array_reverse(explode("\n",ob_get_clean())));
+	require_once("/svn/svnroot/Applications/tlasort.php");
+	usort($r,"tlasort");
+	return $r;
+}
+function getepictrans($konto) {
+	require_once("/svn/svnroot/Applications/ansi-color.php");
+	echo set("Posteringer:\n","inverse");
+	$cmd =("LEDGER_SORT=account,date LEDGER_ACCOUNT_WIDTH=16 LEDGER_PAYEE_WIDTH=22 php /svn/svnroot/Applications/newl.php register ^\"$konto\" ");
+	system($cmd);
+	echo "\n";
+	echo set("Summasummarum:\n","inverse");
+	$cmd =("LEDGER_SORT=account,date LEDGER_ACCOUNT_WIDTH=16 LEDGER_PAYEE_WIDTH=22 php /svn/svnroot/Applications/newl.php balance ^\"$konto\" -E");
+	echo "\n";
+	system($cmd);
+
+
+	echo set("Kvartalsvis:\n","inverse");
+	$cmd =("LEDGER_SORT=account,date LEDGER_ACCOUNT_WIDTH=16 LEDGER_PAYEE_WIDTH=22 php /svn/svnroot/Applications/newl.php register ^\"$konto\" --quarterly");
+	system($cmd);
+	echo "\n";
+
+	echo set("Slægtninge:\n","inverse");
+	$cmd =("LEDGER_SORT=account,date LEDGER_PAYEE_WIDTH=22 php /svn/svnroot/Applications/newl.php balance ^\"$konto\" --flat --related -E");
+	system($cmd);
+	echo "\n";
+
+}
+
+function getpivot() {
+	global $begin; global $end;
+	global $mode;
+	ob_start();
+	$cmd =("php /svn/svnroot/Applications/newl.php print|hledger --depth=2 --begin=$begin --end=$end -f /dev/stdin balance $mode");
+	system($cmd);
+	return ob_get_clean();
+	$r = array_filter(array_reverse(explode("\n",ob_get_clean())));
+	return $r;
+}
+function screensizemode() {
+	$cols = exec("tput cols");
+	if ($cols < 80)
+		return "--yearly";
+	else if ($cols < 180) 
+		return "--quarterly";
+	else
+		return "--monthly";
+}
