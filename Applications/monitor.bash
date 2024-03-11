a5c83c4bf8b919a3e5b5f750c19507aabe749244
Jørgen <olsenit@gmail.com>
2024-03-11 13:07:51 +0100

    Behøver ikke lyd på monitorering

--- Applications/monitor.bash
+++ Applications/monitor.bash
@@ -1,12 +1,6 @@
 y=0
-freq=500 ;inotifywait -m -r / |while read i
+freq=500 ;inotifywait -m -r /data/ |while read i
 	do 
-			if  (( $freq % 100 == 0)) 
-			then
-				beep -f $freq -l 25 >/dev/null
-#				bash /svn/svnroot/Applications/music.bash 8888 $freq 0.75&
-
-			fi
 			date=$(date)
 			echo "$date $i" > /dev/stderr
 			let "freq=freq+25"
