597b92b327a57229a938ed57bf3bf7eed055a9cc
Jørgen <olsenit@gmail.com>
2024-04-21 01:04:18 +0200

    A little fault tolerance

--- Applications/short.php
+++ Applications/short.php
@@ -1,5 +1,5 @@
 <?php
-	function fgc($s) {return file_get_contents($s);}
+	function fgc($s) {if (file_exists($s)) return file_get_contents($s);else return "";}
 	function eon() { error_reporting(0);};
 	function eoff() { error_reporting(E_ALL);};
 ?>
