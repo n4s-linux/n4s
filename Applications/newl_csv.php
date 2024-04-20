1a30b6f763906e1111aa646b6a1eb5f023d40104
Jørgen <olsenit@gmail.com>
2024-04-21 00:59:26 +0200

    Ledger hack to print reference

--- Applications/newl_csv.php
+++ Applications/newl_csv.php
@@ -1,7 +1,7 @@
 <?php
 function getcsv($begin,$end,$tpath) {
 		$data = array();
-		$csv = shell_exec("tpath=$tpath LEDGER_BEGIN=$begin LEDGER_END=$end php /svn/svnroot/Applications/newl.php csv --no-pager -X -B ");
+		$csv = shell_exec("unset LEDGER_DEPTH;LEDGER_DEPTH=10 tpath=$tpath LEDGER_BEGIN=$begin LEDGER_END=$end color=none php /svn/svnroot/Applications/newl.php csv --no-pager -X -B");
 		$lines = explode(PHP_EOL, $csv);
 		$array = array();
 		$data = array();
