b52c8b9c117ddc739f84a5c57a55d877087ea493
Jørgen <olsenit@gmail.com>
2024-05-01 18:52:15 +0200

    Translation

--- Applications/scrips/.tags.bash
+++ Applications/scrips/.tags.bash
@@ -1,10 +1,10 @@
 function gettags() {
-        fn=$((ls -ltr $tpath/.tags/|grep -v .scrabble$|grep -v ".regn$"|grep -v ".diff$";echo NY)|fzf --no-sort -e --tac|awk '{print $NF}')
+        fn=$((ls -ltr $tpath/.tags/|grep -v .scrabble$|grep -v ".regn$"|grep -v ".diff$";echo NEW)|fzf --no-sort -e --tac|awk '{print $NF}')
 	#cat /tmp/tagz.tmp
         #cp /tmp/tagz.tmp /tmp/tagz
-	if [ "$fn" == "NY" ] || [ "$fn" == "ny" ] ; then
+	if [ "$fn" == "NEW" ] ; then
 		stty sane
-		echo -n "Indtast nyt tag: " > /dev/tty
+		echo -n "New journal name: " > /dev/tty
 		read fn	 </dev/tty
 		touch "$tpath/.tags/$fn"
 	fi
