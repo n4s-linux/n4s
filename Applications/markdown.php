742b016d1f176a92ffe678eb83e5eb4876a392ad
Jørgen <olsenit@gmail.com>
2024-05-09 01:08:52 +0200

    Work in progress, small step in interpreting the markdown files from json

--- Applications/markdown.php
+++ Applications/markdown.php
@@ -1,5 +1,5 @@
 <?php
-$op = system("whoami");
+$op = exec("whoami");
 
 foreach (getjson($argv[1]) as $curblock) {
 	handleblock($curblock);
@@ -10,16 +10,37 @@ foreach (getjson($argv[1]) as $curblock) {
 function handleblock($block) {
 	if ($block['t'] == "Header") handleheader($block);
 	else if ($block['t'] == "CodeBlock") return handlecode($block);
+	else if ($block["t"] == "BulletList") return handlebullet($block);
 	else return handleunhandled($block);
+}
+function handlebullet($block) {
+	print_r($block);die();
 }
 function handleunhandled($block) {
 	echo "unhandled $block[t]\n";
 }
 function handlecode($block) {
-	print_r($block);
+	$str = "";
+	foreach ($block["c"] as $curblock) {
+		if (!is_string($curblock)) continue;
+		$str .= $curblock . "\n";
+	}
+	return trim($str);
+}
+function getheadertext($block) {
+	$str = "";
+	foreach ($block as $curblock) {
+		if ($curblock["t"] == "Str") $str .= $curblock["c"];
+		else if ($curblock["t"] == "Space") $str .= " ";
+		else {
+			echo "unhandled ";
+			print_r($curblock);die();
+		}
+	}
+	return $str;
 }
 function handleheader($block){
-$return getheadertext($block['c'][2]);
+return getheadertext($block['c'][2]);
 return false;
 }
 function getjson($fn) {
