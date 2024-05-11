d737125be49f6f5b6ebeced100ec9cef6b5e30d7
Jørgen <olsenit@gmail.com>
2024-05-11 14:13:51 +0200

    Reversing amounts for nautal amount "direction"

--- Applications/moneyflow.php
+++ Applications/moneyflow.php
@@ -3,6 +3,7 @@
 	function moneyflow() {
 	global $totalsum;
 	$rv = "<center>";
+	$rv .= moneyspec();
 	ob_start();
 	system("color=none php /svn/svnroot/Applications/newl.php csv --related ^Aktiver:Likvider");
 	$data = explode("\n",trim(ob_get_clean()));
@@ -14,7 +15,7 @@
 		$moneyflow[$acc] += $amount;
 		error_reporting(E_ALL);
 	}
-	ksort($moneyflow);
+	asort($moneyflow,SORT_NUMERIC);
 	foreach (array("Egenkapital","Indtægter","Udgifter","Aktiver","Passiver","Fejlkonto") as $curgroup) {
 		$sum = 0;
 		//style="white-space:nowrap;"
@@ -24,6 +25,8 @@
 		foreach ($moneyflow as $key => $val) {
 			if (substr($key,0,strlen($curgroup)) == $curgroup) {
 				$key = substr($key,strlen($curgroup)+1);
+				$x = explode(":",$key);
+				$key = (isset($x[1])) ? "$x[0]⇒$x[1]" : $x[0];
 				$pval = number_format($val,0,",",".");
 				$totalsum += $val;
 				$ptotal = number_format($totalsum,0,",",".");
@@ -35,7 +38,6 @@
 		$rv .="<tr><td width=550><b><u>Total $curgroup</u></b></td><td width=100><b><u><p align=right>$psum</p></u></b></td><td><p align=right>$ptotal</p></td>";
 		$rv .= "</table><br>";
 	}
-	$rv .= moneyspec();
 	$rv .= "</center>";
 
 
@@ -43,7 +45,7 @@
 	}
 	function moneyspec() {
 	global $totalsum;
-		$rv .= "<br><h3>Specifikation</h3>";
+		$rv .= "<br><h3>Bevægelser</h3>";
 	$rv .= "<table style='white-space:nowrap' width=600 class=table>";
 		$rv .= "<td width=550>Konto</td><td width=100><p align=right>Ind/Ud</p></td><td><p align=right>Nulkontrol</p></td></tr>";
 	ob_start();
@@ -54,16 +56,18 @@
 		$d = str_getcsv($curline);
 		error_reporting(0);
 		$acc = $d[3];
-		$amount = -$d[5];
+		$amount = $d[5];
 		error_reporting(0);
 		$moneyflow[$acc] += $amount;
 		error_reporting(E_ALL);
 	}
 
 	foreach ($moneyflow as $key => $val) {
-		$totalsum += $val;
+		if ($val == 0) continue;
+		$totalsum -= $val;
 		$pval = number_format($val,0,",",".");
 		$ptotal = number_format($totalsum,0,",",".");
+		$key = substr($key,strlen("Aktiver:Likvider:"));
 		$rv .= "<tr><td width=550>$key</td><td width=100><p align=right>$pval</p></td><td><p align=right>$ptotal</p></td></tr>";
 	}
 	$rv .= "</table>";
