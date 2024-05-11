cf7f645e823406d066ec333b5630c10634b34d09
Jørgen <olsenit@gmail.com>
2024-05-11 13:15:55 +0200

    Make result transactions "unrelated"

--- Applications/addresult.php
+++ Applications/addresult.php
@@ -1,14 +1,21 @@
 <?php
 	function addresult($x) {
 		$income = 0;
-		foreach ($x as &$ct) {
+		foreach ($x as $ct) {
+			$nt = array();
 			foreach ($ct['Transactions'] as $curtrans) {
 				if ($curtrans['Amount'] == 0) continue;
 				if (substr($curtrans['Account'],0,strlen("Indtægter")) == "Indtægter" || substr($curtrans['Account'],0,strlen("Udgifter")) == "Udgifter") {
-					$ct['Transactions'][] = array('Account'=>'Egenkapital:Periodens resultat','Amount'=>$curtrans['Amount'],'id'=>"virt");
-					$ct['Transactions'][] = array('Account'=>'Resultatdisponering:Periodens resultat','Amount'=>-floatval($curtrans['Amount']),'id'=>"virt");
+					if (empty($nt)) {
+						$nt = $ct;
+						unset($nt["Transactions"]);
+					}
+					$nt["Description"] = "🖥️ $nt[Description]";
+					$nt['Transactions'][] = array('Account'=>'Egenkapital:Periodens resultat','Amount'=>$curtrans['Amount'],'id'=>"virt");
+					$nt['Transactions'][] = array('Account'=>'Resultatdisponering:Periodens resultat','Amount'=>-floatval($curtrans['Amount']),'id'=>"virt");
 				}
 			}
+			if (!empty($nt)) array_push($x,$nt);
 		}
 		return $x;
 	}
