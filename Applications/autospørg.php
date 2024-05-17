c8953954a5cfaf61fd2a9216b4bf4048c7d0c075
Jørgen <olsenit@gmail.com>
2024-05-17 22:30:07 +0200

    Improved fejlkonto email questionaire

--- Applications/autospørg.php
+++ Applications/autospørg.php
@@ -9,26 +9,34 @@
 		if ($data == false) continue;
 		if (!isset($data["Transactions"])) continue;
 		if (!stristr(json_encode($data['Transactions']),"fejl")) continue;
-		if (!isset($data["Comment"]) || $data["Comment"] == "") continue;
+		//if (!isset($data["Comment"]) || $data["Comment"] == "") continue;
 		$ask[dirname($curfile)][] = $data;
 	}
+	$url = "https://github.com/n4s-linux/n4s-gratis-regnskab";
+	$version = "<a href=$url>n4s" . exec("cd /svn/svnroot/;echo $(git log |wc -l)/1000|bc -l|perl -pe 's/ ^0+ | 0+$ //xg'") . "🐧</a>";
 	foreach ($ask as $curcustomer => $questions) {
+		$count = 0;
 		$bn = basename($curcustomer);
-		$html = "<h3>Fejlkonto $bn</h3><br>\n";
-		$html .= "<table border=1><tr><td>Ref</td><td>Date</td><td>Text</td><td>Account</td><td>Contra</td><td>Amount</td><td>Comment</td><td bgcolor=yellow>Answer</td></tr>\n";
+//		$html = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">';
+		$html = "<style>"  .file_get_contents("/svn/svnroot/Applications/bs.css") . "</style>";
+		$html .= "<h3>Autogenereret Fejlkonto $bn - $version</h3><br>\n";
+		$html .= "<table class=table border=1><tr><td>Ref</td><td>Date</td><td>Text</td><td>Account</td><td>Contra</td><td>Amount</td><td>Comment</td><td bgcolor=yellow>Answer</td></tr>\n";
 		foreach ($questions as $curquestion) {
-			if (strtotime($curquestion["Date"]) < strtotime("2023-01-01")) continue; //ignore old postings
+			if (strtotime($curquestion["Date"]) < strtotime("2024-01-01")) continue; //ignore old postings
 			if (!isset($curquestion["Transactions"]) || count($curquestion["Transactions"]) < 1) continue;
 			foreach ($curquestion["Transactions"] as $curtrans) {
 				if (!stristr($curtrans["Account"],"fejl")) continue;
-				$shortacc = end(explode(":",$curtrans["Account"]));
+				$shortacc = (explode(":",$curtrans["Account"]));
+				$shortacc=end($shortacc);
 				$pamount = number_format($curtrans["Amount"],2,",",".");
 				$shortcontra = getcontra($curquestion);
+				if (!isset($curquestion["Comment"]))$curquestion["Comment"] = ""; 
 				$html .= "<tr><td>$curquestion[Reference]&nbsp;</td><td>$curquestion[Date]</td><td>$curquestion[Description]</td><td>$shortacc</td><td>$shortcontra</td><td><p align=right>$pamount</p></td><td>$curquestion[Comment]</td><td bgcolor=yellow>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>\n";
+				$count++;
 			}
 		}
 		$html .= "</table>";
-		sendmail("joo@0lsen.com","Fejlkonto $bn",$html);
+		if ($count>0) sendmail("joo@0lsen.com","Fejlkonto $bn",$html);
 	}
 function sendmail($to,$subject,$message) {
 $headers[] = 'MIME-Version: 1.0';
@@ -44,7 +52,7 @@ mail($to, $subject, $message, implode("\r\n", $headers));
 function getcontra($trans) {
 	$rv = "";
 	foreach ($trans["Transactions"] as $curtrans) {
-		if (!stristr($curtrans["Account"],"fejl")) $rv .= end(explode(":",$curtrans["Account"])). "\n";
+		if (!stristr($curtrans["Account"],"fejl")) { $x = explode(":",$curtrans["Account"]);$x = end($x); $rv .= "$x\n";}
 	}
 	return trim($rv);
 }
