e27ba4e147d78a75c78136baf502750adca29400
Jørgen <olsenit@gmail.com>
2024-05-17 22:32:14 +0200

    ⇒ symbol as delimiter for shortaccs

--- Applications/shortacc.php
+++ Applications/shortacc.php
@@ -2,8 +2,11 @@
 function shortacc($konto) {
         $x = explode(":",$konto) ;
         $retval = "";
+	$i = 0;
         foreach ($x as $word) {
+		if ($i > 0) $retval .= "⇒";
 		$retval .= strtolower(mb_substr($word,0,1));
+		$i++;
         }
         return $retval;
 }
