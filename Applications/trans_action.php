6203f64002e266b5af458370023977d3c0ce15af
Jørgen <olsenit@gmail.com>
2024-05-23 11:06:09 +0200

    Improved transactions editor

--- Applications/trans_action.php
+++ Applications/trans_action.php
@@ -13,6 +13,9 @@ if ($json == null) {
 }
 else {
 	$fzf = "";
+	$fzf .= "Reverse numbers\n";
+	$fzf .= "Invert account order\n";
+	if ($json["Transactions"][0]["Amount"] == $json["Transactions"][1]["Amount"] *-1) $fzf .= "Change amount\n";
 	$i = 0;
 	if (!isset($json["Description"])) $json["Description"] = "";
 	if (!isset($json["Comment"])) $json["Comment"] = "";
@@ -25,18 +28,38 @@ else {
 		$bal += $ct["Amount"];
 		$prettyamount = str_pad(number_format($ct["Amount"],2,".",","),15, " ",STR_PAD_LEFT);
 		$prettybal= str_pad(number_format($bal,2,".",","),15, " ",STR_PAD_LEFT);
-		$fzf .= "💸 [t$i] 💸\t$ct[Account]\t$ct[Func]\t$prettyamount\t$prettybal\n";
+		$fzf .= "💸 [$i] 💸\t$ct[Account]\t$ct[Func]\t$prettyamount\t$prettybal\n";
 		$i++;
 	}
-	$fzf .= "💸 [t$i] 💸\tNew\n";
+	$fzf .= "New Transaction\n";
 	$valg = fzf($fzf,"Select field for changing","--tac -e",true);
 	if ($valg != "") {
-		$x = explode("[",explode("]",$valg)[0])[1];
-		if (substr($x,0,1) == "t") {
-			$json = changetrans($json,substr($x,1));
+		if ($valg == "Invert account order") {
+			$json["Transactions"] = array_reverse($json["Transactions"]);
+		}
+		else if ($valg == "Change amount") {
+			exec("tmux display-popup -h3 -E 'gum input --prompt=\"new amount: \">~/tmp/gumout'");
+			$amount = floatval(str_replace(",",".",trim(file_get_contents("/home/$op/tmp/gumout"))));
+			foreach ($json["Transactions"] as &$ct) {
+				if ($ct["Amount"] < 0) $ct["Amount"] = -$amount;
+				else $ct["Amount"] = $amount;
+			}
+		}
+		else if ($valg == "Reverse numbers") {
+			foreach ($json["Transactions"] as &$ct) {
+				$ct["Amount"] = $ct["Amount"] *-1;	
+			}
+		}
+		else if (substr($valg,0,strlen("💸 [")) == "💸 [") {
+			$x = explode("[",explode("]",$valg)[0])[1];
+			$json = changetrans($json,$x);
+		}
+		else if ($valg == "New Transaction") {
+
 		}
 		else {
-			exec("tmux display-popup -E 'gum input --prompt=\"$x [$json[$x]]: \">~/tmp/gumout'");
+			$x = explode("[",explode("]",$valg)[0])[1];
+			exec("tmux display-popup -h3 -E 'gum input --prompt=\"change $x: \">~/tmp/gumout'");
 			$json[$x] = trim(file_get_contents("/home/$op/tmp/gumout"));
 		}
 	}
@@ -52,12 +75,12 @@ function changetrans($json,$trans){
 		$json["Transactions"][$trans]["Func"] = "";
 		$json["Transactions"][$trans]["Amount"] = "NEW";
 	}
-	exec("tmux display-popup -E 'gum input --prompt=\"" . $json["Transactions"][$trans]["Account"] . " \">~/tmp/gumout'");
+	exec("tmux display-popup -h3 -E 'gum input --prompt=\"" . $json["Transactions"][$trans]["Account"] . " \">~/tmp/gumout'");
 	$json["Transactions"][$trans]["Account"] = trim(file_get_contents("/home/$op/tmp/gumout"));
-	exec("tmux display-popup -E 'php /svn/svnroot/Applications/get_func.php getfunc'");
+	exec("tmux display-popup -h20 -E 'php /svn/svnroot/Applications/get_func.php getfunc'");
 	$json["Transactions"][$trans]["Func"] = trim(file_get_contents("/home/$op/tmp/getfunc"));
 	if ($json["Transactions"][$trans]["Amount"] == "NEW") {
-		exec("tmux display-popup -E 'gum input --prompt=Amount >~/tmp/gumout'");
+		exec("tmux display-popup -h3 -E 'gum input --prompt=Amount >~/tmp/gumout'");
 		$json["Transactions"][$trans]["Amount"] = trim(file_get_contents("/home/$op/tmp/gumout"));
 	}
 	return $json;
