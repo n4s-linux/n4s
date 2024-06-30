4976532aa1dce9e6a3bb83873e7f768ee72cc7ae
Jørgen <olsenit@gmail.com>
2024-06-30 20:38:11 +0200

    Show aliases on even foot as real accounts on account lookup

--- Applications/lookup_account.php
+++ Applications/lookup_account.php
@@ -2,11 +2,21 @@
 require_once("/svn/svnroot/Applications/print_array.php");
 function lookup_acc($accounts,$bal,$alias = "",$multi = "--multi") {
 	$path = getenv("tpath");
+	$af = "$path/aliases";
+	if (file_exists($af)) $af = json_decode(file_get_contents($af),true); else $af = array();
 	$accountstring = "";
 	ob_start();
-	$cmd = ("((updatealiases= noend=1 LEDGER_BEGIN=1900-01-01 color=none php /svn/svnroot/Applications/newl.php accounts;cat /svn/svnroot/Libraries/Kontoplan.txt)|sort|uniq|grep :;echo MANUAL;echo 'Fejlkonto:Ved ikke')|fzf --ansi --tac --header-first --header=\"$alias\" --scrollbar=* $multi --border --preview-label='Seneste posteringer' --preview-window 50%:bottom  --preview 'LEDGER_PAYEE_WIDTH=20 LEDGER_ACCOUNT_WIDTH=15 updatealiases= color=none php /svn/svnroot/Applications/newl.php register ^\"{}\" |tac'");
+	$aout="";
+	foreach ($af as $curalias => $curacc) {
+		$aout .= "🦎 $curalias => $curacc\n";
+	}
+	$op = exec("whoami");
+	file_put_contents("/home/$op/tmp/aliases.out",$aout);
+	$cmd = ("((cat ~/tmp/aliases.out;updatealiases= noend=1 LEDGER_BEGIN=1900-01-01 color=none php /svn/svnroot/Applications/newl.php accounts;cat /svn/svnroot/Libraries/Kontoplan.txt;cat ~/tmp/aliases.out)|sort|uniq|grep :;echo MANUAL;echo 'Fejlkonto:Ved ikke')|fzf --ansi --tac --header-first --header=\"$alias\" --scrollbar=* $multi --border --preview-label='Seneste posteringer' --preview-window 50%:bottom  --preview 'LEDGER_PAYEE_WIDTH=20 LEDGER_ACCOUNT_WIDTH=15 updatealiases= color=none php /svn/svnroot/Applications/newl.php register ^\"{}\" |tac'");
 	system($cmd);
 	$accountstring = trim(ob_get_clean());
+	$accountstring = str_replace("🦎","",$accountstring);
+	$accountstring = trim(explode("=>",$accountstring)[0]);
 	if ($accountstring == "") die("Lookupaccount Interrupted - Aborting...\n");
 	if ($accountstring=="MANUAL") {
 		$valg = fzf("Finans\nDebitor\nKreditor","Pick Account Type");
