9dd43da56b6bcfc39d251b61744afc23474e42a4
Jørgen <olsenit@gmail.com>
2024-07-02 12:12:20 +0200

    Different icons for different kinds of accounts

--- Applications/lookup_account.php
+++ Applications/lookup_account.php
@@ -7,15 +7,20 @@ function lookup_acc($accounts,$bal,$alias = "",$multi = "--multi") {
 	$accountstring = "";
 	ob_start();
 	$aout="";
+	if (!empty($af)) {
 	foreach ($af as $curalias => $curacc) {
-		$aout .= "🦎 $curalias => $curacc\n";
+		$aout .= "🔥 $curalias => $curacc\n";
+	}
 	}
 	$op = exec("whoami");
 	file_put_contents("/home/$op/tmp/aliases.out",$aout);
-	$cmd = ("((cat ~/tmp/aliases.out;updatealiases= noend=1 LEDGER_BEGIN=1900-01-01 color=none php /svn/svnroot/Applications/newl.php accounts;cat /svn/svnroot/Libraries/Kontoplan.txt;cat ~/tmp/aliases.out)|sort|uniq|grep :;echo MANUAL;echo 'Fejlkonto:Ved ikke')|fzf --ansi --tac --header-first --header=\"$alias\" --scrollbar=* $multi --border --preview-label='Seneste posteringer' --preview-window 50%:bottom  --preview 'LEDGER_PAYEE_WIDTH=20 LEDGER_ACCOUNT_WIDTH=15 updatealiases= color=none php /svn/svnroot/Applications/newl.php register ^\"{}\" |tac'");
+	$cmd = ("((cat ~/tmp/aliases.out;updatealiases= noend=1 LEDGER_BEGIN=1900-01-01 LEDGER_END=2099-12-31 color=none noemoji=1 php /svn/svnroot/Applications/newl.php accounts;cat /svn/svnroot/Libraries/Kontoplan.txt|/svn/svnroot/Applications/bookify.bash;cat ~/tmp/aliases.out)|sort|uniq|grep :;echo 🌠MANUAL;echo '🤔Fejlkonto:Ved Ikke')|fzf --ansi --tac --header-first --header=\"$alias\" --scrollbar=* $multi --border --preview-label='Seneste posteringer' --preview-window 50%:bottom  --preview 'LEDGER_PAYEE_WIDTH=20 LEDGER_ACCOUNT_WIDTH=15 updatealiases= color=none php /svn/svnroot/Applications/newl.php register ^\"{}\" |tac'");
 	system($cmd);
 	$accountstring = trim(ob_get_clean());
-	$accountstring = str_replace("🦎","",$accountstring);
+	$accountstring = str_replace("🔥","",$accountstring);
+	$accountstring = str_replace("📑","",$accountstring);
+	$accountstring = str_replace("🌠","",$accountstring);
+	$accountstring = str_replace("🤔","",$accountstring);
 	$accountstring = trim(explode("=>",$accountstring)[0]);
 	if ($accountstring == "") die("Lookupaccount Interrupted - Aborting...\n");
 	if ($accountstring=="MANUAL") {
