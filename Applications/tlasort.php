8d171bdb9ca69e77ccb055d38437f756b321fcc1
Jørgen <olsenit@gmail.com>
2024-10-26 20:38:17 +0200

    Improvement on tlasort i hope

--- Applications/tlasort.php
+++ Applications/tlasort.php
@@ -1,8 +1,10 @@
 <?php
 function tlasort($a,$b) {
-	$a = explode(":",$a)[0];
-	$b = explode(":",$b)[0];
-        $score = array('Indtægter'=>0,'Udgifter'=>1,'Resultatdisponering'=>2,'Aktiver'=>4,'Egenkapital'=>5,'Passiver'=>6);
-	if (isset($scora[$a])) return $score[$a]; else return 999;
+	$a = trim(explode(":",$a)[0]);
+	$b = trim(explode(":",$b)[0]);
+        $score = array('Indtægter'=>0,'Udgifter'=>1,'Resultatdisponering'=>2,'Aktiver'=>4,'Egenkapital'=>5,'Passiver'=>6,"Fejlkonto"=>10);
+	if (isset($score[$a])) $sa = $score[$a]; else $sa = 999;
+	if (isset($score[$b])) $sb = $score[$b]; else $sb = 999;
+	return ($sa > $sb) ? -1 :1;
 }
 ?>
