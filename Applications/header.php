86354a22bc1b7f99a8cfb685f80a875950f503f4
Jørgen <olsenit@gmail.com>
2024-12-10 17:45:08 +0100

    Web header file

---
+++ Applications/header.php
@@ -0,0 +1,9 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Menu</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
