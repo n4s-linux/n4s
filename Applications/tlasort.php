77c4eb89125d24dd833fd0078573e262d8dfa022
Jørgen <olsenit@gmail.com>
2024-12-10 17:41:04 +0100

    Reversal of the tla sort order - needed for web

--- Applications/tlasort.php
+++ Applications/tlasort.php
@@ -5,6 +5,6 @@ function tlasort($a,$b) {
         $score = array('Indtægter'=>0,'Udgifter'=>1,'Resultatdisponering'=>2,'Aktiver'=>4,'Egenkapital'=>5,'Passiver'=>6,"Fejlkonto"=>10);
 	if (isset($score[$a])) $sa = $score[$a]; else $sa = 999;
 	if (isset($score[$b])) $sb = $score[$b]; else $sb = 999;
-	return ($sa > $sb) ? -1 :1;
+	return ($sa < $sb) ? -1 :1;
 }
 ?>
