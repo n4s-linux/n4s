2baaccbf1f28a6046a2abff4f819096497caf01a
Jørgen <olsenit@gmail.com>
2024-12-10 17:37:55 +0100

    Reversing the section order

--- Applications/mkd.bash
+++ Applications/mkd.bash
@@ -12,7 +12,7 @@ if [ "$2" == "sections" ]; then
 grep "^# " "$1"|sed s/^#\ //
 
 elif [ "$2" == "select" ]; then
-grep "^# " "$1"|sed s/^#\ //|fzf --multi --header="chose markdown headings to print - PRESS SPACE TO SELECT ALL" --query="!Fakturering !Færdig !Hvidvask !Mapper !Samkøring !Stamdata !Logins" --bind space:select-all|while read heading
+grep "^# " "$1"|sed s/^#\ //|fzf --tac --multi --header="chose markdown headings to print - PRESS SPACE TO SELECT ALL" --query="!Fakturering !Færdig !Hvidvask !Mapper !Samkøring !Stamdata !Logins" --bind space:select-all|while read heading
 do
 	php /svn/svnroot/Applications/getmdheading.php "$1" "$heading" 
 done
