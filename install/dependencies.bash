19116f875c2b6b6d0bef85226f266809475008b3
Jørgen <olsenit@gmail.com>
2024-12-29 15:55:45 +0100

    typo

--- install/dependencies.bash
+++ install/dependencies.bash
@@ -3,7 +3,7 @@ function exitprg() {
 	echo "Error @ Dependencies: $1"
 	exit 1
 }
-ls /svn/svnroot/Libraries/Dependencies_debian.txt|while read i
+cat /svn/svnroot/Libraries/Dependencies_debian.txt|while read i
 do
 apt-get y "$i" || exitprg "couldnt install $i"
 done
