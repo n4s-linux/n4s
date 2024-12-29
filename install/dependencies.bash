9a44c592aa02f911b04ccc6b860925578256e2f9
Jørgen <olsenit@gmail.com>
2024-12-29 16:05:32 +0100

    Bugfix install dep word for word

--- install/dependencies.bash
+++ install/dependencies.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 function exitprg() {
-	echo "Error @ Dependencies: $1"
-	exit 1
+    echo "Error @ Dependencies: $1"
+    exit 1
 }
-cat /svn/svnroot/Libraries/Dependencies_debian.txt|while read i
-do
-apt-get -y install "$i" || exitprg "couldnt install $i"
+
+# Read words from the file
+cat /svn/svnroot/Libraries/Dependencies_debian.txt | tr ' \n' '\n ' | while read -r i; do
+    apt-get -y install "$i" || exitprg "couldn't install $i"
 done
+
