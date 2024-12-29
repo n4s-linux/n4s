1b396dfe290b52c86be5b3b19287ccf62d82e327
Jørgen <olsenit@gmail.com>
2024-12-29 16:28:39 +0100

    dont forget to update before run

--- install/dependencies.bash
+++ install/dependencies.bash
@@ -7,6 +7,7 @@ function exitprg() {
 }
 
 # Read words from the file
+apt-get update
 cat /svn/svnroot/Libraries/Dependencies_debian.txt | tr ' \n' '\n ' | while read -r i; do
     apt-get -y install "$i" || exitprg "couldn't install $i"
 done
