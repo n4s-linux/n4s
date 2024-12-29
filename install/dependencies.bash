3af151f5c8236481fdbefbbdc2ca14bef5d6ad5e
Jørgen <olsenit@gmail.com>
2024-12-29 15:32:33 +0100

    dependencies

--- install/dependencies.bash
+++ install/dependencies.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+function exitprg() {
+	echo "Error @ Dependencies: $1"
+	exit 1
+}
 ls /svn/svnroot/Libraries/Dependencies_Debian.txt|while read i
 do
 apt-get y "$i" || exitprg "couldnt install $i"
